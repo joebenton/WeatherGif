253b6b6b7aa25ef4f99975085d7e307d37127318
Joe Benton <joe.benton@zeta.net>
2017-01-25 16:32:10 +0000

    add Forecast view controller and presenter

---
+++ WeatherGif/UI/Forecast/ForecastPresenter.swift
@@ -0,0 +1,66 @@
+//
+//  ForecastPresenter.swift
+//  WeatherGif
+//
+//  Created by Joe on 25/01/2017.
+//  Copyright © 2017 Joe Benton. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol ForecastView {
+    func setForecast(forecast: Forecast)
+    func requestLocationAuthorization()
+    func startUpdatingLocation()
+    func stopUpdatingLocation()
+}
+
+class ForecastPresenter {
+    
+    fileprivate var view: ForecastView?
+    fileprivate var dataManager: DataManager?
+    fileprivate var currentLocation: CLLocation?
+    fileprivate var forecast: Forecast?
+    
+    init(view: ForecastView, dataManager: DataManager? = DataManager()) {
+        self.view = view
+        self.dataManager = dataManager
+    }
+    
+    func getWeatherForecast() {
+        guard let lat = currentLocation?.coordinate.latitude, let lng = currentLocation?.coordinate.longitude else {
+            return
+        }
+        
+        dataManager?.getForecast(withLatitude: lat, withLongitude: lng, completion: { (forecast, errorMessage) in
+            if let forecast = forecast {
+                self.forecast = forecast
+                
+                self.view?.setForecast(forecast: forecast)
+            } else if let errorMessage = errorMessage {
+                print(errorMessage)
+            }
+        })
+    }
+    
+    func getLocation() {
+        view?.requestLocationAuthorization()
+    }
+    
+    func locationAuthorizationChanged(status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse || status == .authorizedAlways {
+            view?.startUpdatingLocation()
+        }
+    }
+    
+    func didUpdateLocations(locations: [CLLocation]) {
+        if locations.count > 0 {
+            self.currentLocation = locations.first
+            view?.stopUpdatingLocation()
+            
+            getWeatherForecast()
+        }
+    }
+}
+
---
+++ WeatherGif/UI/Forecast/ForecastViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ForecastViewController.swift
+//  WeatherGif
+//
+//  Created by Joe on 25/01/2017.
+//  Copyright © 2017 Joe Benton. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ForecastViewController: UIViewController {
+
+    var presenter: ForecastPresenter?
+    var locationManager: CLLocationManager?
+    
+    @IBOutlet weak var weatherGifWebView: UIWebView!
+    @IBOutlet weak var weatherTypeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        
+        presenter = ForecastPresenter(view: self)
+        presenter?.getLocation()
+    }
+}
+
+extension ForecastViewController: ForecastView {
+    func setForecast(forecast: Forecast) {
+        weatherTypeLabel.text = forecast.weatherSummary
+        temperatureLabel.text = forecast.temperature
+    }
+    
+    func requestLocationAuthorization() {
+        locationManager?.requestWhenInUseAuthorization()
+    }
+    
+    func startUpdatingLocation() {
+        locationManager?.startUpdatingLocation()
+    }
+    
+    func stopUpdatingLocation() {
+        locationManager?.stopUpdatingLocation()
+    }
+}
+
+extension ForecastViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        presenter?.locationAuthorizationChanged(status: status)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.count > 0 {
+            presenter?.didUpdateLocations(locations: locations)
+        }
+    }
+}
