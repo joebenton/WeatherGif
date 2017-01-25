ee96afc00dba811cc4ba9b35e15893d56d9c4246
Joe Benton <joe.benton@zeta.net>
2017-01-25 16:32:28 +0000

    add forecast model

---
+++ WeatherGif/Data/Model/Forecast.swift
@@ -0,0 +1,14 @@
+//
+//  Forecast.swift
+//  WeatherGif
+//
+//  Created by Joe on 25/01/2017.
+//  Copyright © 2017 Joe Benton. All rights reserved.
+//
+
+import Foundation
+
+struct Forecast {
+    let weatherSummary: String
+    let temperature: String
+}
