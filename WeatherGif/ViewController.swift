b738c61a2a75cc6e23bd3f05e3e056f1b07e1e86
Joe Benton <joe.benton@zeta.net>
2017-01-25 14:57:02 +0000

    Initial Commit

---
+++ WeatherGif/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherGif
+//
+//  Created by Joe on 25/01/2017.
+//  Copyright © 2017 Joe Benton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
